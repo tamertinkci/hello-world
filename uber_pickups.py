695b57a6013472efa2f5e200d25770622ce20461
tamertinkci <130481110+tamertinkci@users.noreply.github.com>
2024-04-19 14:16:17 +0200

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -8,6 +8,7 @@ DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
+@st.cache_data
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
@@ -19,4 +20,4 @@ data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
 data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
+data_load_state.text("Done! (using st.cache_data)")
